49a03050d461d83c17b3e509a7f46c030d845178
Kenny Ortega <kennyaoc@gmail.com>
2021-10-19 18:50:04 -0400

    Complete calculator interface

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,16 +1,28 @@
 import './Calculator.css';
 
-function Item(item) {
-  return (
-    <div>{item}</div>
-  );
-}
-
 function Calculator() {
   return (
     <div className="container">
-      <div className="result">NaN</div>
-      <Item item="AC" />
+      <div className="result"><span className="character">NaN</span></div>
+      <div className="item"><span className="character">AC</span></div>
+      <div className="item"><span className="character">+/-</span></div>
+      <div className="item"><span className="character">%</span></div>
+      <div className="item operator"><span className="character">÷</span></div>
+      <div className="item"><span className="character">7</span></div>
+      <div className="item"><span className="character">8</span></div>
+      <div className="item"><span className="character">9</span></div>
+      <div className="item operator"><span className="character">x</span></div>
+      <div className="item"><span className="character">4</span></div>
+      <div className="item"><span className="character">5</span></div>
+      <div className="item"><span className="character">6</span></div>
+      <div className="item operator"><span className="character">-</span></div>
+      <div className="item"><span className="character">1</span></div>
+      <div className="item"><span className="character">3</span></div>
+      <div className="item"><span className="character">3</span></div>
+      <div className="item operator"><span className="character">+</span></div>
+      <div className="item zero"><span className="character">0</span></div>
+      <div className="item"><span className="character">.</span></div>
+      <div className="item operator"><span className="character">=</span></div>
     </div>
   );
 }
