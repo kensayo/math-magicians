02cd49727ece75147af53bfd49e6c340760f7980
Kenny Ortega <kennyaoc@gmail.com>
2021-10-22 18:22:59 -0400

    refactor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,43 @@
 import './Calculator.css';
-import React from 'react';
-import { useState } from 'react';
-// import calculate from '../logic/calculate';
+import { useState, React } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [data, setObj] = useState({ total: '0', next: null, operation: null });
+  const { total, next, operation } = data;
 
-  const [data, update] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-  update (input) {
-    console.log(data);
-    console.log(input);
-  }
+  const update = (input) => {
+    setObj(calculate(data, input));
+  };
 
   return (
     <div className="container">
       <div className="result">
-        <span id="operator" />
-        <span id="result">0</span>
+        <span id="result">
+          {total}
+          {operation}
+          {next}
+        </span>
       </div>
-      <div className="item"><button type="button" value="AC" className="character" onClick={update('A/C')}>AC</button></div>
-      <div className="item"><button type="button" value="+/-" className="character">+/-</button></div>
-      <div className="item"><button type="button" value="%" className="character">%</button></div>
-      <div className="item operator"><button type="button" value="÷" className="character">÷</button></div>
-      <div className="item"><button type="button" value="7" className="character">7</button></div>
-      <div className="item"><button type="button" value="8" className="character">8</button></div>
-      <div className="item"><button type="button" value="9" className="character">9</button></div>
-      <div className="item operator"><button type="button" value="x" className="character">x</button></div>
-      <div className="item"><button type="button" value="4" className="character">4</button></div>
-      <div className="item"><button type="button" value="5" className="character">5</button></div>
-      <div className="item"><button type="button" value="6" className="character">6</button></div>
-      <div className="item operator"><button type="button" value="-" className="character">-</button></div>
-      <div className="item"><button type="button" value="1" className="character">1</button></div>
-      <div className="item"><button type="button" value="2" className="character">2</button></div>
-      <div className="item"><button type="button" value="3" className="character">3</button></div>
-      <div className="item operator"><button type="button" value="+" className="character">+</button></div>
-      <div className="item zero"><button type="button" value="0" className="character">0</button></div>
-      <div className="item"><button type="button" value="." className="character">.</button></div>
-      <div className="item operator"><button type="button" value="=" className="character">=</button></div>
+      <div className="item"><button type="button" value="AC" onClick={() => update('AC')}>AC</button></div>
+      <div className="item"><button type="button" value="+/-" onClick={() => update('+/-')}>+/-</button></div>
+      <div className="item"><button type="button" value="%" onClick={() => update('%')}>%</button></div>
+      <div className="item operator"><button type="button" value="÷" onClick={() => update('÷')}>÷</button></div>
+      <div className="item"><button type="button" text="7" value="7" onClick={() => update('7')}>7</button></div>
+      <div className="item"><button type="button" value="8" onClick={() => update('8')}>8</button></div>
+      <div className="item"><button type="button" value="9" onClick={() => update('9')}>9</button></div>
+      <div className="item operator"><button type="button" value="x" onClick={() => update('x')}>x</button></div>
+      <div className="item"><button type="button" value="4" onClick={() => update('4')}>4</button></div>
+      <div className="item"><button type="button" value="5" onClick={() => update('5')}>5</button></div>
+      <div className="item"><button type="button" value="6" onClick={() => update('6')}>6</button></div>
+      <div className="item operator"><button type="button" value="-" onClick={() => update('-')}>-</button></div>
+      <div className="item"><button type="button" value="1" onClick={() => update('1')}>1</button></div>
+      <div className="item"><button type="button" value="2" onClick={() => update('2')}>2</button></div>
+      <div className="item"><button type="button" value="3" onClick={() => update('3')}>3</button></div>
+      <div className="item operator"><button type="button" value="+" onClick={() => update('+')}>+</button></div>
+      <div className="item zero"><button type="button" value="0" onClick={() => update('0')}>0</button></div>
+      <div className="item"><button type="button" value="." onClick={() => update('.')}>.</button></div>
+      <div className="item operator"><button type="button" value="=" onClick={() => update('=')}>=</button></div>
     </div>
   );
 }
