1e5fc009f63f7170a0ef51d5b881e175dbdc2156
Kenny Ortega <kennyaoc@gmail.com>
2021-10-27 00:23:13 -0400

    Add Footer

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 > "Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:
 
 - Make simple calculations.
-- Read a random math-related quote.
+- Read a random quote.
+
+[]('./)
 
 ### 👷🏻 Built using
     - React
--- src/App.js
+++ src/App.js
@@ -6,48 +6,52 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
 import Quote from './components/Quote';
+import Footer from './components/Footer';
 import './App.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <nav className="navbar navbar-expand navbar-light bg-light">
-          <div className="container-fluid">
-            <NavLink className="nav-link link-dark fw-bold fs-4" to="/home">Math Magicians</NavLink>
-            <div className="navbar-collapse">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end w-100 pe-5">
-                <li className="nav-item px-3">
-                  <NavLink activeClassName="active" className="nav-link" to="/home">Home</NavLink>
-                </li>
-                <li className="nav-item px-3">
-                  <span className="nav-link">|</span>
-                </li>
-                <li className="nav-item px-3">
-                  <NavLink activeClassName="active" className="nav-link" to="/calculator">Calculator</NavLink>
-                </li>
-                <li className="nav-item px-3">
-                  <span className="nav-link">|</span>
-                </li>
-                <li className="nav-item px-3">
-                  <NavLink activeClassName="active" className="nav-link" to="/quote">Quote</NavLink>
-                </li>
-              </ul>
+      <div>
+        <Router>
+          <nav className="navbar navbar-expand navbar-light bg-light">
+            <div className="container-fluid">
+              <NavLink className="nav-link link-dark fw-bold fs-4" to="/home">Math Magicians</NavLink>
+              <div className="navbar-collapse">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end w-100 pe-5">
+                  <li className="nav-item px-3">
+                    <NavLink activeClassName="fw-bold active" className="nav-link" to="/home">Home</NavLink>
+                  </li>
+                  <li className="nav-item px-3">
+                    <span className="nav-link">|</span>
+                  </li>
+                  <li className="nav-item px-3">
+                    <NavLink activeClassName="fw-bold active" className="nav-link" to="/calculator">Calculator</NavLink>
+                  </li>
+                  <li className="nav-item px-3">
+                    <span className="nav-link">|</span>
+                  </li>
+                  <li className="nav-item px-3">
+                    <NavLink activeClassName="fw-bold active" className="nav-link" to="/quote">Quote</NavLink>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/calculator">
-            <Calculator />
-          </Route>
-          <Route path="/quote">
-            <Quote />
-          </Route>
-        </Switch>
-      </Router>
+          </nav>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/calculator">
+              <Calculator />
+            </Route>
+            <Route path="/quote">
+              <Quote />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
     );
   }
 }
